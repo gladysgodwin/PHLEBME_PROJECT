2362c9a5bdf1a03ef1a7a7299ceb135f90b673ba
Gladys Godwin <gladysgodwin79@gmail.com>
2022-08-19 02:42:11 +0200

    added new changes

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.menu-items');
+
+
+const menuItem = document.querySelectorAll('.menu-item')
+
+menuBtn.addEventListener('click', ()=> {
+    menuBtn.classList.toggle('open');
+    menuItems.classList.toggle('open');
+});
+
+menuItem.forEach(item => {
+
+    
+});
