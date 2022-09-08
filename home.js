ac9223b8ca3a6290902824a8940695ed0d583b00
Gladys Godwin <gladysgodwin79@gmail.com>
2022-09-09 01:16:21 +0200

    added new files

---
+++ home.js
@@ -0,0 +1,39 @@
+const myslide =document.querySelectorAll('.myslider');
+        dot = document.querySelectorAll('.dot');
+
+        let counter = 1;
+        slidefun(counter);
+
+        let timer= setInterval(autoslide, 8000);
+        function autoscale(){
+            counter += 1;
+            slidefun(counter);
+        }
+        function plusSlides(n) {
+            counter += n;
+            slidefun(counter);
+            resetTimer();
+        }
+        function currentSlides(n) {
+            counter = n;
+            slidefun(counter);
+            resetTimer();
+        }
+        function resetTimer(){
+            clearInterval(timer);
+            timer= setInterval(autoslide, 8000);
+        }
+        function slidefun(n) {
+            let i;
+            for(i= 0; i < myslide.length; i++){
+                myslide[i].style.display = "none";
+            }
+        for (i = 0; i< dot.length; i++){
+            dot[i].classList.remove('active');
+        }
+        if(n > myslide.length){
+            counter = myslide.length;
+        }
+        myslide[counter -1].style.display ="block";
+        dot[counter - 1].classList.add('active');
+    }
--- main.js
+++ main.js
@@ -13,3 +13,5 @@ menuItem.forEach(item => {
 
     
 });
+
+
